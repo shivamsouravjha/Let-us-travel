fcf67f94571668dace56c1a6f1edf2b17e6afc84
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-01-31 02:30:13 +0530

    Frontend placesitem

--- DBMS/Frontend/src/places/components/Placeslist.js
+++ DBMS/Frontend/src/places/components/Placeslist.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import Card from '../../shared/Components/UIElements/Card';
+import Placeitem from './Placeitem';
 import './Placelist.css';
 const Placelist =props=>{
-    return
+    if(props.items.length ===0){
+        return( <div className="place-list center">
+            <card>
+                <h2> 
+                    no such places....
+                    <button>
+                        Share Place
+                    </button>
+                </h2>
+            </card>
+        </div>);
+    }
+    return <ul className="place-list">
+        {props.items.map(place=> <Placeitem 
+                    key={place.id}
+                    id={place.id}
+                    image={place.imageUrl}
+                    title1={place.title}
+                    description={user.description}
+                    address={user.address}
+                    creatorId={place.creator}
+                    coordinates={place.location}
+        />)}
+    </ul>;
 };
 export default Placelist;

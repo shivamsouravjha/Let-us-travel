48eb5fca3e07dfc0ecd6b08af9ea3e4faa77ea08
Shivam Sourav Jha <60891544+shivamsouravjha@users.noreply.github.com>
2021-02-11 23:01:11 +0530

    Delete Backend directory

--- Backend/models/error.js
+++
@@ -1,7 +0,0 @@
-class Httperror  extends Error{
-    constructor(message,errorCode){
-        super(message);
-        this.code= errorCode;  //adds a 'code' property 
-    }
-}
-module.exports = Httperror;
